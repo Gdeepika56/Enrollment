0fea9e35e8f1fd68339bff59cd049803e6b12f53
Gdeepika56 <dg1201@usnh.edu>
2025-01-22 13:59:10 -0500

    add user layout and css

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
-from application import app
+from application import app, db
 from flask import render_template, request, json, Response
+from application.models import User, Course, Enrollment
 
 courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
 
@@ -38,3 +39,14 @@ def api(idx=None):
         jdata = courseData[int(idx)]
     
     return Response(json.dumps(jdata), mimetype="application/json")
+
+
+
+@app.route("/user")
+def user():
+    # if not User.objects(user_id=1).first():
+    #     User(user_id=1, first_name="Felix", last_name="lee", email="lee.felix@uta.com",password="1234").save()
+    # if not User.objects(user_id=2).first():
+    #     User(user_id=2, first_name="thv", last_name="kim", email="thv30@uta.com", password="v1234").save()
+    users = User.objects.all()
+    return render_template("user.html", users=users)
