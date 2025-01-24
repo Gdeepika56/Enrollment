25c1f97a78c55ccfdfaf503c992fdc79591a9f50
Gdeepika56 <dg1201@usnh.edu>
2025-01-24 17:54:22 -0500

    make few changes in form fields

--- application/forms.py
+++ application/forms.py
@@ -1,17 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from application.models import User
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6,max=15)])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Submit')
 
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password_confrim = PasswordField('Confrim Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(),Length(min=6,max=15)])
+    password_confrim = PasswordField('Confrim Password', validators=[DataRequired(), Length(min=6,max=15), EqualTo('password')])
     first_name = PasswordField('First Name', validators=[DataRequired()])
     last_name = PasswordField('Last Name', validators=[DataRequired()])
     submit = SubmitField('Register Now')    
+
+def validate_Email(self, email):
+    user = User.objects(email=email.data).first()
+    if user:
+        raise ValidationError('Email is already in use. Pick another one.')
