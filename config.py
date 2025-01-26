da5cbce4b24e91b8a4b807271207250e6d7f7954
Gdeepika56 <dg1201@usnh.edu>
2025-01-26 16:23:06 -0500

    import os details to config file

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "'\xd4z\xc6P\xceUC\xae\xdd\xb2\x8f!N,\x83\x11'"
 
     MONGODB_SETTINGS = { 'db' : 'UTA_Enrollment',
                         'host' : 'mongodb://localhost:27017/UTA_Enrollment'
