5853af5cd9c5b315b25ad22874db7ce74b5cf1f4
Gdeepika56 <dg1201@usnh.edu>
2025-01-26 23:35:31 -0500

    create REST API and api check in postman

--- application/routes.py
+++ application/routes.py
@@ -1,9 +1,48 @@
-from application import app, db
-from flask import render_template, request, json, Response, redirect, flash, url_for, session
+from application import app, db, api
+from flask import render_template, request, json, jsonify, Response, redirect, flash, url_for, session
 from application.models import User, Course, Enrollment
 from application.forms import LoginForm, RegisterForm
+from flask_restx import Resource
 
 courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
+
+###################################################
+@api.route('/api/', '/api/')
+class GetAndPost(Resource):
+
+    #GET ALL
+    def get(self):
+        return jsonify(User.objects.all())
+    
+    #POST
+    def post(self):
+        data = api.payload
+        user = User(user_id=data['user_id'], email=data['email'], first_name=data['first_name'], last_name=data['last_name'])
+        user.set_password(data['password'])
+        user.save()
+        return jsonify(User.objects(user_id=data['user_id']))
+
+@api.route('/api/<idx>')
+class GetUpdateDelete(Resource):
+
+    #GET ONE
+    def get(slef, idx):
+        return jsonify(User.objects(user_id=idx))
+    
+    #PUT
+    def put(self, idx):
+        data = api.payload
+        User.objects(user_id=idx).update(**data)
+        return jsonify(User.objects(user_id=idx))
+    
+    #DELETE
+    def delete(self, idx):
+        User.objects(user_id=idx).delete()
+        return jsonify("User is deleted!")
+    
+
+
+###################################################
 
 @app.route("/")
 @app.route("/index")
@@ -128,15 +167,15 @@ def enrollment():
 
 
 
-@app.route("/api/")
-@app.route("/api/<idx>")
-def api(idx=None):
-    if(idx == None):
-        jdata = courseData
-    else:
-        jdata = courseData[int(idx)]
+# @app.route("/api/")
+# @app.route("/api/<idx>")
+# def api(idx=None):
+#     if(idx == None):
+#         jdata = courseData
+#     else:
+#         jdata = courseData[int(idx)]
     
-    return Response(json.dumps(jdata), mimetype="application/json")
+#     return Response(json.dumps(jdata), mimetype="application/json")
 
 
 
